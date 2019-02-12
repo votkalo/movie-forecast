09dfd93a4357232f31792d9faf9f1a412856474e
Kirill Votkalo <kirillvotkalo@gmail.com>
2019-02-13 00:24:23 +0300

    Refactor Locality data object

--- movie-forecast-commons/movie-forecast-commons-data-object/src/main/kotlin/com/vo/movie/forecast/commons/data/Locality.kt
+++ movie-forecast-commons/movie-forecast-commons-data-object/src/main/kotlin/com/vo/movie/forecast/commons/data/Locality.kt
@@ -1,3 +1,3 @@
 package com.vo.movie.forecast.commons.data
 
-data class Locality(val name: String, val movieScheduleURL: String)
+data class Locality(val name: String, val alternativeName: String, val moviesScheduleURL: String)
