3d9b199e893e27a79afc6507fe731394e7faa896
Kirill Votkalo <kirillvotkalo@gmail.com>
2019-02-20 22:40:52 +0300

    Add originTitle and year in MovieSchedule.

--- movie-forecast-parser/movie-forecast-parser-api/movie-forecast-parser-api-schedule/src/main/kotlin/com/vo/movie/forecast/parser/api/schedule/dto/MovieSchedule.kt
+++ movie-forecast-parser/movie-forecast-parser-api/movie-forecast-parser-api-schedule/src/main/kotlin/com/vo/movie/forecast/parser/api/schedule/dto/MovieSchedule.kt
@@ -1,5 +1,7 @@
 package com.vo.movie.forecast.parser.api.schedule.dto
 
 data class MovieSchedule(val title: String,
+                         val originalTitle: String?,
+                         val year: String,
                          val scheduleURL: String,
                          val cinemas: List<CinemaSchedule>)
